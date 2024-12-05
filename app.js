7a4bb781aab60a1a9198ea9359aa5d384a23c5ea
Nolan REYMOND <nolan.reymond@saintmichelannecy.fr>
2024-12-05 08:57:17 +0100

    Code Vmrt fini

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-// Références
 const sidebar = document.getElementById('history-sidebar');
 const toggleHistory = document.getElementById('toggle-history');
 const closeHistory = document.getElementById('close-history');
-const historyList = document.getElementById('history-list');
 
-// Bouton pour afficher la barre latérale
 toggleHistory.addEventListener('click', () => {
+    console.log('Bouton ouvrir cliqué');
     sidebar.classList.add('active');
 });
 
-// Bouton pour fermer la barre latérale
 closeHistory.addEventListener('click', () => {
+    console.log('Bouton fermer cliqué');
     sidebar.classList.remove('active');
 });
-
-// Fonction pour ajouter un calcul à l'historique
-function addToHistory(operation, result) {
-    const entry = document.createElement('p');
-    entry.textContent = `${operation} = ${result}`;
-    historyList.appendChild(entry);
-}
